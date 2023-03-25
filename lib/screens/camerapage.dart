bdcb9c9c4fe4fa6da8ae4e481964663c7202c806
reikihingorani <reikihingorani@gmail.com>
2023-03-25 16:34:36 -0500

    added camera and video player (with preview), didn't connect to any page yet

---
+++ lib/screens/camerapage.dart
@@ -0,0 +1,83 @@
+import 'package:catch_up/screens/videopage.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart'; 
+
+
+// create a class for camerapage
+// initialization
+class CameraPage extends StatefulWidget {
+  const CameraPage({Key? key}) : super(key: key);
+
+  @override
+  _CameraPageState createState() => _CameraPageState();
+}
+
+// new class for loading page 
+class _CameraPageState extends State<CameraPage> {
+  bool _isLoading = true; 
+  bool _isRecording = false;
+  _initCamera() async {
+  final cameras = await availableCameras();
+  final front = cameras.firstWhere((camera) => camera.lensDirection == CameraLensDirection.front);
+  _cameraController = CameraController(front, ResolutionPreset.max);
+  await _cameraController.initialize();
+  setState(() => _isLoading = false);
+}
+  late CameraController _cameraController;
+
+ @override
+void initState() {
+  super.initState();
+  _initCamera();
+}  
+_recordVideo() async {
+  if (_isRecording) {
+    final file = await _cameraController.stopVideoRecording();
+    setState(() => _isRecording = false);
+    final route = MaterialPageRoute(
+      fullscreenDialog: true,
+      builder: (_) => VideoPage(filePath: file.path),
+    );
+    Navigator.push(context, route);
+  } else {
+    await _cameraController.prepareForVideoRecording();
+    await _cameraController.startVideoRecording();
+    setState(() => _isRecording = true);
+  }
+}
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isLoading) { // if camera page is loading, display loading page
+      return Container(
+        color: Colors.white,
+        child: const Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      return Center(
+  child: Stack(
+    alignment: Alignment.bottomCenter,
+    children: [
+      CameraPreview(_cameraController),
+      Padding(
+        padding: const EdgeInsets.all(25),
+        child: FloatingActionButton(
+          backgroundColor: Colors.red,
+          child: Icon(_isRecording ? Icons.stop : Icons.circle),
+          onPressed: () => _recordVideo(),
+        ),
+      ),
+    ],
+  ),
+);
+    }
+  }
+}
